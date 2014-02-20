09c5695d76bd7c30619fa7372814e35f1208333c
Uday Verma <uday.karan@gmail.com>
2014-02-19 21:28:52 -0600

    Fix template resource issue which wouldn't let v0.0.1 run

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dakait "0.0.1"
+(defproject dakait "0.0.2"
   :description "A tool to download files from your FTP/SFTP servers in an organized way."
   :url "https://github.com/verma/dakait"
   :dependencies [[org.clojure/clojure "1.5.1"]
--- src/dakait/views.clj
+++ src/dakait/views.clj
@@ -1,15 +1,22 @@
 (ns dakait.views
   (:use 
     dakait.config
+    [clojure.java.io :only (resource)]
     [clostache.parser :only (render)]))
 
 (defn render-template [file data]
   (render (slurp file) data))
 
+(defn render-resource [file data]
+  (-> file
+      resource
+      slurp
+      (render data)))
+
 (defn index-page []
-  (render-template "templates/index.mustache" {:title "Hello"
+  (render-resource "templates/index.mustache" {:title "Hello"
                                                :server-name (config :server-name) }))
 
 (defn tags-page []
-  (render-template "templates/tags.mustache" {:title "Tags"
+  (render-resource "templates/tags.mustache" {:title "Tags"
                                               :server-name (config :server-name)}))
