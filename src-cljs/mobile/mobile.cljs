56e5df268421e3467926526d77d2776fb1e7edbc
Uday Verma <uday.karan@gmail.com>
2014-03-15 15:57:10 -0500

    Preliminary actions

--- src-cljs/mobile/mobile.cljs
+++ src-cljs/mobile/mobile.cljs
@@ -1,7 +1,9 @@
 (ns dakait.mobile
-    (:require [ajax.core :refer [GET]]
-              [om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [ajax.core :refer [GET]]
+            [cljs.core.async :refer [put! <! chan]]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
 
 (def app-state (atom {:files [] :path "."}))
 
@@ -17,27 +19,41 @@
 
 (defn file-item [file owner]
   (reify
-    om/IRender
-    (render [this]
-      (dom/li #js {:className "table-view-cell"}
-        (dom/a #js {:className "navigate-right"}
-               (get file "name")
-               (dom/button #js {:className "btn"}
-                  (dom/span #js {:className "icon icon-edit"})))))))
+    om/IRenderState
+    (render-state [this {:keys [comm]}]
+      (let [name (get file "name")]
+        (dom/li #js {:className "table-view-cell"}
+          (dom/a #js {:className "navigate-right"}
+                 name
+                 (dom/button #js {:className "btn btn-link"
+                                  :onClick (fn [e] 
+                                             (.preventDefault e)
+                                             (.stopPropagation e)
+                                             (put! comm {:type :tag
+                                                               :value name}))}
+                    (dom/span #js {:className "icon icon-edit"}))))))))
+
+(defn handle-event [type app value]
+  (log "Type: " type ", Value: " value))
 
 (defn file-browser [{:keys [files path] :as app} owner]
   (reify
     om/IWillMount
     (will-mount [_]
-      (log "Element mounted, calling API")
+      (let [comm (chan)]
+        (om/set-state! owner :comm comm)
+        (go (while true
+              (let [{:keys [type value]} (<! comm)]
+                (handle-event type app value)))))
       (GET "/a/files" {:params {:path path}
                        :handler (fn [r]
                                   (update-files r app))}))
     om/IRenderState
-    (render-state [_ _]
+    (render-state [_ state]
       (if (seq files)
         (apply dom/ul #js {:className "table-view"}
-               (om/build-all file-item (:files app)))
+               (om/build-all file-item (:files app)
+                             {:init-state state}))
         (dom/div nil
                  (str "No Files"))))))
 
