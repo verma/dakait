8c5d944e7c577fe4a5546daf296196f27220bd69
Uday Verma <uday.karan@gmail.com>
2014-03-16 10:49:42 -0500

    Version bump to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dakait "0.0.9"
+(defproject dakait "0.1.0"
   :description "A tool to download files from your FTP/SFTP servers in an organized way."
   :url "https://github.com/verma/dakait"
   :dependencies [[org.clojure/clojure "1.5.1"]
