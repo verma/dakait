f34807d670f5e8ac8337f9a4c9257f48626909f0
Uday Verma <uday.karan@gmail.com>
2014-03-30 10:10:33 -0500

    Update dependencies using lein-ancient

--- project.clj
+++ project.clj
@@ -3,24 +3,25 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/verma/dakait"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/core.match "0.2.1"]
                  [compojure "1.1.6"]
                  [clj-ssh "0.5.7"]
                  [ring/ring-devel "1.2.2"]
                  [ring/ring-core "1.2.2"]
-                 [http-kit "2.1.16"]
-                 [org.clojure/clojurescript "0.0-2156"]
+                 [http-kit "2.1.18"]
+                 [org.clojure/clojurescript "0.0-2197"]
                  [org.clojure/data.json "0.2.4"]
                  [de.ubercode.clostache/clostache "1.3.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [javax.jmdns/jmdns "3.4.1"]
-                 [me.raynes/conch "0.5.0"]
+                 [me.raynes/conch "0.5.1"]
                  [reagent "0.4.2"]
                  [crate "0.2.5"]
                  [jayq "2.5.0"]]
-  :plugins [[lein-cljsbuild "1.0.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]
+            [lein-ancient "0.5.5"]]
 
   :main dakait.main
 
